62396ed108090a59ff6cdb86022d05f9652005b1
𝕷𝖀𝕾𝕿┊𝐃𝐄𝐕 ⽮ <valerylusty@gmail.com>
2026-06-19 20:35:36 -0500

    Create ping.js

---
+++ commands/ping.js
@@ -0,0 +1,18 @@
+import stylizedChar from "../utils/fancy.js"
+
+export async function pingTest(client, message) {
+    const remoteJid = message.key.remoteJid
+    const start = Date.now()
+
+    await client.sendMessage(remoteJid, { text: "📡 Pinging..." }, { quoted: message })
+
+    const latency = Date.now() - start
+
+    await client.sendMessage(remoteJid, {
+        text: stylizedChar(
+            `🚀 Clan Destin Network\n\n` +
+            `Latency: ${latency} ms\n\n` +
+            `CLAN DESTIN CD`
+        )
+    }, { quoted: message })
+}
